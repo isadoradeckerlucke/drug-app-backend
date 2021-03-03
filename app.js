7e7fff610458611ed9d05a75b4b4a7120f29b474
Isadora Decker-Lucke <isadoradl5@gmail.com>
2021-03-03 11:43:36 -0700

    added body parser

--- app.js
+++ app.js
@@ -7,6 +7,14 @@ const cors = require("cors");
 const morgan = require("morgan");
 const app = express();
 const path = require("path");
+var bodyParser = require("body-parser");
+app.use(bodyParser.json()); // to support JSON-encoded bodies
+app.use(
+  bodyParser.urlencoded({
+    // to support URL-encoded bodies
+    extended: true,
+  })
+);
 
 // app.use(express.static(path.join(__dirname, "frontend/build")));
 
